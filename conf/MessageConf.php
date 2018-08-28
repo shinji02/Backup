4b192ec8e34ff88c80a5262fce2326305cc46288
Jovann <jovannserreau@gmail.com>
2018-08-28 14:46:25 +0200

    Ajout des messages

---
+++ conf/MessageConf.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace conf;
+
+class MessageConf {
+	
+	static $TITLE_BACKP_SUCESS_ONE = "Backup du site {sitename} à été réaliser avec succées";
+	static $MESSAGE_BACKUP_SUCESS_ONE ="Le backup du site: {sitename} à été réaliser avec succées";
+	static $TITLE_BACKUP_ALL = "Backup des site ont été réaliser avec succées";
+	static $MESSAGE_BACKUP_SUCESS_ALL = "Les backup ont été résilier avec succées";
+	static $TITLE_BACKUP_ECHEC = "Echec du backup du site {sitename}";
+	static $MESSAGE_BACKUP_ECHE = "Le backup du site {sitename} à echouer: Raison: {Raison}";
+}
